2daf21486b9ed9c892adb7c90f58661b8078bb9f
Heike Hofmann <hofmann@iastate.edu>
2011-09-15 06:45:42 -0500

    small fixes to chemicals section - in particular to code

--- code/PAH code.R
+++ code/PAH code.R
@@ -1,27 +1,40 @@
 #PAH code
 #choose lrf_WATER_benchmarks in data folder
 setwd("C:\\Users\\Owner\\Documents\\csda-writeup")
-surf.water <- read.csv("data/lrf_WATER_benchmarks.csv")
-#format data
-surf.water$DATE<- as.character(surf.water$DATE)
-surf.water$DATE <- as.Date(surf.water$DATE, "%m/%d/%Y")
-water.bench1 <- ddply(surf.water, .(LONGITUDE, LATITUDE, SUBSTANCE), summarize,
-	avg.Acute.Potency.Ratio = mean(Acute.Potency.Ratio),
-	avg.Chronic.Potency.Ratio = mean(Chronic.Potency.Ratio))
 
-water.bench <- ddply(water.bench1, .(LONGITUDE, LATITUDE), summarize,
-	Acute.Benchmark.Value = sum(avg.Acute.Potency.Ratio),
-	Chronic.Benchmark.Value = sum(avg.Chronic.Potency.Ratio))
+################# Libraries Used #################
+library(ggplot2)
+
+################# Read in Data #################
+surfwater <- read.csv("data/lrf_WATER_benchmarks.csv")
 
 #choose lrf_SEDIMENT_benchmarks in data folder
 sediment <- read.csv("data/lrf_SEDIMENT_benchmarks.csv")
 
 #choose OC in the data folder
 OC <- read.csv("data/OC.csv")
-#format data
 
+################# Format Data #################
+surfwater$DATE <- as.Date(as.character(surfwater$DATE), "%m/%d/%Y")
+
+# at each location summarize each substance
+waterbench_substance <- ddply(surfwater, .(LONGITUDE, LATITUDE, SUBSTANCE), summarize,
+	avg.Acute.Potency.Ratio = mean(Acute.Potency.Ratio),
+	avg.Chronic.Potency.Ratio = mean(Chronic.Potency.Ratio))
+
+# get overall benchmark values at each location
+waterbench <- ddply(waterbench_substance, .(LONGITUDE, LATITUDE), summarize,
+	Acute.Benchmark.Value = sum(avg.Acute.Potency.Ratio),
+	Chronic.Benchmark.Value = sum(avg.Chronic.Potency.Ratio))
+
+
+#format data
+#### what is going on here? ####
 OC <- ddply(OC, .(LONGITUDE, LATITUDE), transform,
-	avgOC = mean(OCRESULT))
+	 avgOC = mean(OCRESULT))
+	 
+OC <- ddply(OC, .(LONGITUDE, LATITUDE), transform,
+	  n= length(OCRESULT))
 
 OC.sediment <- merge(OC, sediment, by=c("LONGITUDE","LATITUDE")) 
 OC.sediment <- subset(OC.sediment, avgOC != 0)
@@ -37,12 +50,12 @@ sediment.bench3a <- ddply(sediment.bench3, .(LONGITUDE, LATITUDE, SUBSTANCE), su
 sediment.bench <- ddply(sediment.bench3a, .(LONGITUDE, LATITUDE), summarize,
 			Acute.Benchmark.Value = sum(avg.Acute.Potency.Ratio),
 			Chronic.Benchmark.Value = sum(avg.Chronic.Potency.Ratio))
-sediment.bench3$DATE<- as.character(sediment.bench3$DATE)
-sediment.bench3$DATE <- as.Date(sediment.bench3$DATE, "%m/%d/%Y")
+
+sediment.bench3$DATE <- as.Date(as.character(sediment.bench3$DATE), "%m/%d/%Y")
 sediment.bench3$ratios <- with(sediment.bench3, Chronic.Potency.Ratio/Acute.Potency.Ratio)
 sediment.bench4 <-  na.omit(unique(sediment.bench3[,c("SUBSTANCE", "ratios")]))
-nsub <- length(unique(surf.water$SUBSTANCE))+1
-amax <- max(surf.water$Acute.Potency.Ratio)
+nsub <- length(unique(surfwater$SUBSTANCE))+1
+amax <- max(surfwater$Acute.Potency.Ratio)
 cmax <- 1/mean(sediment.bench3$ratios, na.rm=T)
 acute <- geom_polygon(aes(x=c(0.5,0.5,nsub,nsub), y=c(1,amax+.1,amax+.1,1)), fill="red", alpha=0.1)
 acute_text <- geom_text(aes(x=(nsub+1)/2, y=mean(c(1,amax)), label="acute"), colour="red", size=15, alpha=0.5)
@@ -52,7 +65,7 @@ chronic_data <- data.frame(x=(nsub+1)/2, y=mean(c(1,cmax)), Danger.Level="Carcin
 chronic_text <- geom_text(aes(x,y, label="chronic"), angle=90, colour="yellow", size=15, alpha=0.5, data=chronic_data)
 #geom_path(aes(x=as.numeric(SUBSTANCE), y=1/ratios), data=sediment.bench3)
 
-local.data <- surf.water[,c("SUBSTANCE", "Acute.Potency.Ratio", "Danger.Level")]
+local.data <- surfwater[,c("SUBSTANCE", "Acute.Potency.Ratio", "Danger.Level")]
 local.data <- rbind(local.data, sediment.bench3[,c("SUBSTANCE", "Acute.Potency.Ratio", "Danger.Level")])
 local.data <- subset(local.data, as.character(SUBSTANCE) !="")
 local.data$SUBSTANCE <- factor(local.data$SUBSTANCE)
@@ -63,7 +76,7 @@ water.danger + opts(title = "Chronic and Acute Potency Ratios")
 ggsave("images/chron-acute-ratios.png")
 #------------------------------------------------------------------------------------------------------
 
-total <- rbind(water.bench, sediment.bench) 
+total <- rbind(waterbench, sediment.bench) 
 ggplot() +
  geom_tile(aes(lon, lat, fill=fill), data = sat_map) +
  coord_cartesian(xlim=range(sat_map$lon), ylim=range(sat_map$lat)) +
@@ -90,12 +103,12 @@ ggsave("images/chron-acute-map.png")
 #timeline
 ##needs some adjustments - lost previous code :(
 sediment.bench3$Log_Acute.Potency.Ratio<-log((sediment.bench3$Acute.Potency.Ratio+1))
-surf.water$Log_Acute.Potency.Ratio<-log((surf.water$Acute.Potency.Ratio+1))
+surfwater$Log_Acute.Potency.Ratio<-log((surfwater$Acute.Potency.Ratio+1))
 
 sediment.time <- ggplot() + geom_vline(yintercept=log(2), colour="grey50") + geom_point(aes(x = DATE, y = Log_Acute.Potency.Ratio, colour=Danger.Level), size =5, data = subset(sediment.bench3, RESULT!=0)) + 
 scale_colour_discrete() +labs(y="Log of Acute Potency Ratio", x="Date") + ylim(0,0.6) + opts(title="Sediment Acute Potency Ratios") 
 
-water.time <- ggplot() + geom_vline(yintercept=log(2), colour="grey50") + geom_point(aes(x = DATE, y = Log_Acute.Potency.Ratio, colour=Danger.Level), size =5, data = subset(surf.water, RESULT!=0)) + 
+water.time <- ggplot() + geom_vline(yintercept=log(2), colour="grey50") + geom_point(aes(x = DATE, y = Log_Acute.Potency.Ratio, colour=Danger.Level), size =5, data = subset(surfwater, RESULT!=0)) + 
 scale_colour_discrete() +labs(y="Log of Acute Potency Ratio", x="Date") + ylim(0,0.6) + opts(title="Surface Water Acute Potency Ratios") 
 
 grid.arrange(sediment.time, water.time, ncol = 1)
@@ -103,8 +116,8 @@ ggsave("images/acute-timeline.png")
 
 # need to still fix height and width
 sediment.bench4 <-  na.omit(unique(sediment.bench3[,c("SUBSTANCE", "ratios")]))
-nsub <- length(unique(surf.water$SUBSTANCE))+1
-amax <- max(surf.water$Acute.Potency.Ratio)
+nsub <- length(unique(surfwater$SUBSTANCE))+1
+amax <- max(surfwater$Acute.Potency.Ratio)
 cmax <- 1/mean(sediment.bench3$ratios, na.rm=T)
 acute <- geom_polygon(aes(x=c(0.5,0.5,nsub,nsub), y=c(1,amax+.1,amax+.1,1)), fill="red", alpha=0.1)
 acute_text <- geom_text(aes(x=(nsub+1)/2, y=mean(c(1,amax)), label="acute"), colour="red", size=15, alpha=0.5)
@@ -114,7 +127,7 @@ chronic_data <- data.frame(x=(nsub+1)/2, y=mean(c(1,cmax)), Danger.Level="Carcin
 chronic_text <- geom_text(aes(x,y, label="chronic"), angle=90, colour="yellow", size=15, alpha=0.5, data=chronic_data)
 #geom_path(aes(x=as.numeric(SUBSTANCE), y=1/ratios), data=sediment.bench3)
 
-local.data <- surf.water[,c("SUBSTANCE", "Acute.Potency.Ratio", "Danger.Level")]
+local.data <- surfwater[,c("SUBSTANCE", "Acute.Potency.Ratio", "Danger.Level")]
 local.data <- rbind(local.data, sediment.bench3[,c("SUBSTANCE", "Acute.Potency.Ratio", "Danger.Level")])
 local.data <- subset(local.data, as.character(SUBSTANCE) !="")
 local.data$SUBSTANCE <- factor(local.data$SUBSTANCE)
