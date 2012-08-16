d06102c2e3e944a87e5c4f7cac15908805814fbd
Lendie Follett <lfollett@iastate.edu>
2012-08-16 08:58:12 -0500

    lrf

--- code/PAH code.R
+++ code/PAH code.R
@@ -105,7 +105,7 @@ ggplot()+
 geom_point(aes(x = LONGITUDE, y = LATITUDE, colour = id, size = id), data= data) +
 theme_grey() +labs(x = "Longitude", y = "Latitude") +
 scale_size_manual("Level", values = c("d1" = 1.5, "d2" = 3, "d3" = 2),breaks = c("d1", "d2", "d3"), legend = F) + 
-scale_colour_manual("Level", values = c("d1" = "#990000", "d2" = "#FF6600", "d3" = "#FFCC00"),breaks = c("d1", "d2", "d3"), labels = c("Samples", "Chronic", "Acute")) +
+scale_colour_manual("Level", values = c("d3" = "#FF1E00", "d2" = "#FFB100", "d1" = "#00BF32"),breaks = c("d1", "d2", "d3"), labels = c("Samples", "Chronic", "Acute")) +
 opts(legend.direction = "vertical", legend.position = "right") 
 ggsave("images/chron-acute-map.png")
 
@@ -123,7 +123,7 @@ scale_colour_discrete() +labs(y="Log of Acute Potency Ratio", x="Date") + ylim(0
 
 ggplot() + geom_point(aes(x = DATE, y = Log_Acute.Potency.Ratio, colour=Danger.Level), size =3, data = subset(sediment.bench3, RESULT!=0)) +
 geom_point(aes(x = DATE, y = Log_Acute.Potency.Ratio, colour=Danger.Level), size =3, data = subset(surfwater, RESULT!=0)) +
-theme_grey() + anger_level2 + labs(y = "Log of Acute Potency Ratio", x = "Date") + geom_hline(yintercept = log(2), colour = "grey50") +
+theme_grey() + danger_level2 + labs(y = "Log of Acute Potency Ratio", x = "Date") + geom_hline(yintercept = log(2), colour = "grey50") +
 geom_text(aes(x = DATE, y = Log_Acute.Potency.Ratio, label = SUBSTANCE, colour = Danger.Level), hjust = -.1, data = subset(surfwater, Log_Acute.Potency.Ratio >= log(2)))
 
 #grid.arrange(sediment.time, water.time, ncol = 1)
